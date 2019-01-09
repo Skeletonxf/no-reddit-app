3070d6b99ffaa40df515c8f63c28bcda4c96a481
Skeletonxf <skeletonxf@gmail.com>
2019-01-09 13:56:31 +0000

    Initial commit

---
+++ no-reddit-app.js
@@ -0,0 +1,12 @@
+let style = document.createElement('style')
+style.type = 'text/css'
+style.innerHTML = `
+.XPromoPill {
+  display: none;
+}
+
+.TopButton.pulse {
+  display: none;
+}
+`
+document.head.appendChild(style)
