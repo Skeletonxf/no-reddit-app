4a03ee083c44a560114829fbdf88cab74bc4b12b
Skeletonxf <skeletonxf@gmail.com>
2020-05-30 12:32:20 +0100

    Fix bloated 'compact' mobile web interface

--- no-reddit-app.js
+++ no-reddit-app.js
@@ -8,5 +8,31 @@ style.innerHTML = `
 .TopButton.pulse {
   display: none;
 }
+
+.PostHeader__post-descriptor-line-overflow {
+  padding-top: 0 !important;
+  padding-bottom: 0 !important;
+}
+
+.PostHeader__post-title-line {
+  font-size: 13px !important;
+}
+
+.PostFooter {
+  padding-top: 3px !important;
+  padding-bottom: 3px !important;
+}
+
+.PostContent.size-compact {
+  margin-top: -5px !important;
+}
+
+.CommentsPage {
+  font-size: 13px;
+}
+
+.Post .PostHeader__post-title-line {
+  padding-top: 8px;
+}
 `
 document.head.appendChild(style)
